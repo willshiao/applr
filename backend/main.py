0e52f679b6a1c4073006176a65774be733386a61
ckong007 <ckong007@ucr.edu>
2021-01-30 15:09:33 -0800

    database

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ import json
 
 app = Flask(__name__)
 
-con = psycopg2.connect(database="postgres", user="postgres", password="rlppa", host="34.83.221.162", port="5432")
+con = psycopg2.connect(database="postgres", user="postgres", password="", host="34.83.221.162", port="5432")
 print("Database opened successfully")
 
 secret = ''
