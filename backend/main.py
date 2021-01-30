186a8a6db2f0a658187273d66c20af957c29c344
ckong007 <ckong007@ucr.edu>
2021-01-30 13:37:06 -0800

    save route to database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 import psycopg2
 from flask import Flask, request
+import json
+
 app = Flask(__name__)
 
-con = psycopg2.connect(database="postgres", user="postgres", password="", host="34.83.221.162", port="5432")
+con = psycopg2.connect(database="postgres", user="postgres", password="rlppa", host="34.83.221.162", port="5432")
 print("Database opened successfully")
 
 @app.route('/')
@@ -28,3 +30,16 @@ def user_login():
     if row is None or row[1] != password:
         return { 'status': 'fail', 'message': 'Incorrect username or password!' }
     return { 'status': 'success', 'token': 'todo' }
+
+@app.route('/save', methods = ['POST'])
+def save(): 
+    with open('message.json', 'r') as f:
+        data = f.read()
+        json_data = json.loads(data)
+
+        for i in json_data: 
+            print(i['name'])
+            cur = con.cursor()
+            cur.execute("INSERT INTO applr.fields (user_id, description, value, type) VALUES (%s, %s, %s, %s) ON CONFLICT (user_id, description) DO UPDATE SET value = %s", (3, i['name'], 'Caaarroolllyyynn', 'input', i['name'],))
+            con.commit()
+    return { 'status': 'success'}
