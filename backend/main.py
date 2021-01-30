bbf1426a5764a9603acfe7b821c39387a8f49557
Theodore Shiao <theo.shiao@gmail.com>
2021-01-30 12:36:05 -0800

    added registration

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ import psycopg2
 from flask import Flask, request
 app = Flask(__name__)
 
-con = psycopg2.connect(database="postgres", user="postgres", password="", host="34.83.221.162", port="5432")
+con = psycopg2.connect(database="postgres", user="postgres", password="rlppa", host="34.83.221.162", port="5432")
 print("Database opened successfully")
 
 @app.route('/')
@@ -28,3 +28,20 @@ def user_login():
     if row is None or row[1] != password:
         return { 'status': 'fail', 'message': 'Incorrect username or password!' }
     return { 'status': 'success', 'token': 'todo' }
+
+@app.route('/register', methods = ['POST'])
+def user_registration():
+   body = request.json
+   if body is None:
+      return { 'status': 'fail', 'message': 'Missing body' }
+   username, password = body['username'], body['password']
+   cur = con.cursor()
+   cur.execute("SELECT username FROM applr.users WHERE username = %s", (username,))
+   row = cur.fetchone()
+   if row:
+      return { 'status': 'fail', 'message': 'Username already exists' } 
+
+   cur = con.cursor()
+   cur.execute("INSERT INTO applr.users (username, password) VALUES (%s, %s)", (username, password))
+   con.commit()
+   return { 'status': 'success' }
