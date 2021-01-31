c5845998022693cccf09e7e4983cf9434d9e511a
Theodore Shiao <theo.shiao@gmail.com>
2021-01-31 02:17:47 -0800

    hooked up

--- frontend/signup.js
+++ frontend/signup.js
@@ -1,10 +1,11 @@
+// import swal from 'sweetalert2';
 const signUpForm = document.getElementById("signup-form");
 const signupBtn = document.getElementById("signup-btn");
 const confirmPassword = document.getElementById("confirm-signup-password");
 const signupPassword = document.getElementById("signup-password");
-
 const noMatch = document.getElementById("signup-no-match");
 noMatch.style.display = "none";
+const myStorage = window.localStorage
 
 confirmPassword.addEventListener("change", () => {
     if (confirmPassword.value !== signupPassword.value) {
@@ -15,13 +16,41 @@ confirmPassword.addEventListener("change", () => {
     }
 });
 
-signupBtn.addEventListener("click", (e)=>{
+signUpForm.addEventListener("submit", (e)=>{
     e.preventDefault();
+    console.log("hello")
     const username = signUpForm.username.value;
     const password = signUpForm.password.value;
     const confirmPassword = signUpForm.confirmPassword.value;
-    if (password === confirmPassword) {
-        console.log("sign up success");
-        window.location.href = "./applr.html";
-    }
+    console.log(username, password)
+    fetch("http://localhost:5000/register", {
+        method: "POST",
+        body: JSON.stringify({
+            username: username,
+            password: password
+        }),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(res => res.json())
+    .then(function (data) {
+        let token = data.token
+        myStorage.setItem('applrToken', token)
+        if (data.status === "success") {
+            noMatch.style.display = "none"
+            window.location.href = "../extension/confirmation.html"
+            // swal({
+            //     title: "Signed up!",
+            //     icon: "success",
+            //     button: "Close",
+            // })
+            // .then(funtion() {
+            //     window.location.href = "./applr.html"
+            // })
+        } else {
+            noMatch.style.display = "block"
+        }
+    })
 });
